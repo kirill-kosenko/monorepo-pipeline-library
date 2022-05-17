b0e53faf101a28a52e4338e72d541aff69ae2bca
kirill-kosenko <kirill.kosenko@ardas.biz>
2022-05-18 00:21:37 +0300

    add debug

--- vars/monorepo.groovy
+++ vars/monorepo.groovy
@@ -3,6 +3,7 @@
  */
 def call() {
     String repositoryName = env.JOB_NAME.split('/')[1]
+    println "=====================> $repositoryName"
     String rootFolderPath = "Generated/$repositoryName"
 
     List<String> jenkinsfilePaths = provisionItems(rootFolderPath, env.GIT_URL)
