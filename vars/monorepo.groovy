6512d65654118617c7afda2ebd186231f41855fd
kirill-kosenko <kirill.kosenko@ardas.biz>
2022-05-18 00:47:17 +0300

    load

--- vars/monorepo.groovy
+++ vars/monorepo.groovy
@@ -5,6 +5,8 @@ def call() {
     String repositoryName = "monorepo-sample";
     String rootFolderPath = "Generated/$repositoryName"
 
+    def provision = load "provisionJenkinsItems.groovy"
+
     List<String> jenkinsfilePaths = provisionItems(rootFolderPath, env.GIT_URL)
     List<String> multibranchPipelinesToRun = findMultibranchPipelinesToRun(jenkinsfilePaths)
     runPipelines(rootFolderPath, multibranchPipelinesToRun)
