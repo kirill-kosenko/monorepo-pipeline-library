1868cf5aca7b573156699c02099556e7fe7edd97
kirill-kosenko <kirill.kosenko@ardas.biz>
2022-05-18 03:12:25 +0300

    update library

--- vars/monorepo.groovy
+++ vars/monorepo.groovy
@@ -108,6 +108,7 @@ def runPipelines(String rootFolderPath, List<String> multibranchPipelinesToRun) 
     parallel(multibranchPipelinesToRun.inject([:]) { stages, multibranchPipelineToRun ->
         stages + [("Build $multibranchPipelinesToRun"): {
             def pipelineName = "$rootFolderPath/$multibranchPipelinesToRun/${URLEncoder.encode(env.CHANGE_BRANCH ?: env.GIT_BRANCH, 'UTF-8')}"
+            println "...................$pipelineName"
             // For new branches, Jenkins will receive an event from the version control system to provision the
             // corresponding Pipeline under the Multibranch Pipeline item. We have to wait for Jenkins to process the
             // event so a build can be triggered.
